ea1b889bc030ba931fcc8164614b4660b3b278be
Fred Hebert <mononcqc@ferd.ca>
2023-12-29 16:58:54 -0500

    default regex usage to support unicode

--- src/markdown.erl
+++ src/markdown.erl
@@ -1016,7 +1016,7 @@ closingdiv([$>| T], Acc) -> Acc2 = flatten(reverse(Acc)),
 closingdiv([H|T], Acc)   -> closingdiv(T, [H | Acc]).
 
 get_url(String) -> HTTP_regex = "^(H|h)(T|t)(T|t)(P|p)(S|s)*://",
-                   case re:run(String, HTTP_regex) of
+                   case re:run(String, HTTP_regex, [unicode]) of
                        nomatch    -> not_url;
                        {match, _} -> get_url1(String, [])
                    end.
@@ -1031,7 +1031,7 @@ get_url1([H | T], Acc)       -> get_url1(T, [H | Acc]).
 
 get_email_addie(String) ->
     Snip_regex = ">",
-    case re:run(String, Snip_regex) of
+    case re:run(String, Snip_regex, [unicode]) of
         nomatch                -> not_email;
         {match, [{N, _} | _T]} ->
             {Possible, [$> | T]} = lists:split(N, String),
@@ -1040,7 +1040,7 @@ get_email_addie(String) ->
                 ++ "@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+"
                 ++ "(?:[a-zA-Z]{2}|com|org|net|gov|mil"
                 ++ "|biz|info|mobi|name|aero|jobs|museum)",
-            case re:run(Possible, EMail_regex) of
+            case re:run(Possible, EMail_regex, [unicode]) of
                 nomatch    -> not_email;
                 {match, _} -> {{email, Possible}, T}
             end
